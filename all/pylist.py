741ba313ec537827908bd9cbf9e21ed686957ee4
Root-4CE <alexandr3323@gmail.com>
2023-02-08 15:42:21 +0300

    123sass

---
+++ all/pylist.py
@@ -0,0 +1,25 @@
+l = [input()]
+print(l)
+
+def is_majority(l):
+
+  if len(l) > 0:
+      print('ok')
+  else:
+      return False
+
+print(is_majority(l))
+
+
+# def is_majority():
+
+# ##Check for empty list
+
+# if len(l) > 0:
+#     print('OK')
+# else:
+#     return False
+
+# ##Function of sravnenie for majority
+
+# return True
